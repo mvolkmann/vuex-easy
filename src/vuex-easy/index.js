a1d91ca563500aca2a13572e78ef7d9716157cdc
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-01-20 14:53:37 -0600

    initial work to add validation

--- src/vuex-easy/index.js
+++ src/vuex-easy/index.js
@@ -9,40 +9,75 @@ export {default as RadioButtons} from './RadioButtons';
 export {default as Select} from './Select';
 export {default as TextArea} from './TextArea';
 
+const MSG_PREFIX = 'vuex-easy method ';
+
 Vue.use(Vuex);
 
-let store;
+let options, store;
 
-export function createStore(initialState) {
+const defaultOptions = {persist: true};
+
+export function createStore(initialState, opts = {persist: defaultOptions}) {
+  options = opts;
   store = new Vuex.Store({
     strict: true,
     state: initialState,
     mutations: {
       decrement(state, {path, delta}) {
+        validatePath('decrement', path);
+        validateNumber('decrement', 'delta', delta);
+        validateNumber('decrement', path, get(state, path));
         update(state, path, n => n - delta);
       },
       delete(state, path) {
+        validatePath('delete', path);
         state = omit(state, path);
       },
       filter(state, {path, fn}) {
+        validatePath('filter', path);
+        validateArray('filter', path, get(state, path));
+        validateFunction('filter', fn);
         update(state, path, arr => arr.filter(fn));
       },
       increment(state, {path, delta}) {
+        validatePath('increment', path);
+        validateNumber('increment', 'delta', delta);
+        validateNumber('increment', path, get(state, path));
         update(state, path, n => n + delta);
       },
       map(state, {path, fn}) {
+        validatePath('map', path);
+        validateArray('map', path, get(state, path));
+        validateFunction('map', fn);
         update(state, path, arr => arr.map(fn));
       },
       push(state, {path, values}) {
+        validatePath('push', path);
+        validateArray('push', path, get(state, path));
         get(state, path).push(...values);
       },
       set(state, {path, value}) {
+        validatePath('set', path);
         set(state, path, value);
       },
       toggle(state, path) {
+        validatePath('toggle', path);
+        if (options.validate) {
+          const value = get(state, path);
+          const type = typeof value;
+          if (type !== 'boolean' && type !== 'undefined') {
+            throw new Error(
+              MSG_PREFIX +
+                'toggle requires a path to a boolean value, but found' +
+                type
+            );
+          }
+        }
         set(state, path, !get(state, path));
       },
       transform(state, {path, fn}) {
+        validatePath('transform', path);
+        validateFunction('transform', fn);
         update(state, path, fn);
       }
     },
@@ -90,4 +125,42 @@ export const vxe = {
   transform(path, fn) {
     store.commit('transform', {path, fn});
   }
+};
+
+let validateArray = (methodName, path, value) => {
+  if (Array.isArray(value)) return;
+  throw new Error(
+    MSG_PREFIX +
+      methodName +
+      ' requires an array, but ' +
+      path +
+      ' value is ' +
+      value
+  );
+};
+
+let validateFunction = (methodName, value) => {
+  if (typeof value === 'function') return;
+  throw new Error(
+    MSG_PREFIX + methodName + ' requires a function, but got ' + value
+  );
+};
+
+let validateNumber = (methodName, path, value) => {
+  if (typeof value === 'number') return;
+  throw new Error(
+    MSG_PREFIX +
+      methodName +
+      ' requires a number, but ' +
+      path +
+      ' value is ' +
+      value
+  );
+};
+
+let validatePath = (methodName, path) => {
+  if (typeof path === 'string') return;
+  throw new Error(
+    MSG_PREFIX + methodName + ' requires a string path, but got ' + path
+  );
 };
